31c3019c6139a2bb7352e59a035e76e28bcf9680
Johans Eduardo Mendoza Mannings <a288156@uach.mx>
2015-11-08 19:22:54 -0700

    derecho examen
    Enrique moreno velo 275066
    Johans Eduardo Mendoza Mannings 288156
    derecho a examen

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#comp1").click(function () {
+    $("#p1").slideToggle("fast");
+  });
+  $("#comp2").click(function () {
+    $("#p2").slideToggle("fast");
+  });
+  $("#comp3").click(function () {
+    $("#p3").slideToggle("fast");
+  });
+  });
